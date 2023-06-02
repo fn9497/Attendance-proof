c5df51f1961a7a855627e5ea856fc7487ba66da8
fatima nasser <105967075+fn9497@users.noreply.github.com>
2023-06-02 17:38:03 +0300

    Update views.py

--- Tracker/views.py
+++ Tracker/views.py
@@ -18,5 +18,13 @@ class LecturViewSet(viewsets.ModelViewSet):
 class AttendanceViewSet(viewsets.ModelViewSet):
     queryset = Attendance.objects.all()
     serializer_class = AttendanceSerializer
-
+    filter_backends=[DjangoFilterBackend]
+    filterset_fields=['attend','lecture']
+    
+    def create(self, request, *args, **kwargs):
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        Attendance.objects.create(lecture=serializer.validated_data['lecture'],student=serializer.validated_data['student'],attend=serializer.validated_data['attend'])
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
+        
     
