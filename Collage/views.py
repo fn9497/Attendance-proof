bb6ce7d24e4f882b2541a609e0a5e09cf13ce207
fatima nasser <105967075+fn9497@users.noreply.github.com>
2023-06-03 03:23:00 +0300

    Update views.py

--- Collage/views.py
+++ Collage/views.py
@@ -5,14 +5,23 @@ from .serializers import *
 # Create your views here.
 
 
-class CourseViewSet(viewsets.ModelViewSet):
-    queryset = Course.objects.all()
-    serializer_class = CourseSerializer
+class CourseViewSet(viewsets.ReadOnlyModelViewSet):
+     queryset=Course.objects.all()
+     serializer_class = StudentCourseSerializer
+
+     def get_queryset(self):
+        student_id = self.request.user.id
+        student_course_vs = StudentCourseViewSet(student_id)
+        courses = student_course_vs.get_courses()
+        return courses
 
 
 class StudentCourseViewSet(viewsets.ModelViewSet):
     queryset = StudentCourse.objects.all()
     serializer_class = StudentCourseSerializer
+    def get(self,request):
+        student=Student.objects.get(id=self.student_id)
+        return student.course.all()
 
 
     def get_serializer_class(self):
