cf1ff882f59833a4c9daacd7612dab7ed3875e5b
fatima nasser <105967075+fn9497@users.noreply.github.com>
2023-06-07 22:36:13 +0300

    Update serializers.py

--- Tracker/serializers.py
+++ Tracker/serializers.py
@@ -1,18 +1,38 @@
 from rest_framework import serializers
 from .models import *
-#from Collage.serializers import StudentSerializer
+from Collage.models import *
+from django.contrib.auth.models import User
+from rest_framework.relations import StringRelatedField
 
+class StudentSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Student
+        fields = ['id','user', 'first_name', 'last_name']
+
+    user = StringRelatedField()
+    first_name = serializers.CharField(source='user.first_name')
+    last_name = serializers.CharField(source='user.last_name')
 
 
 
 class LecturSerializer(serializers.ModelSerializer):
     class Meta:
         model = Lecture
-        fields = '__all__'
-       
+        fields = '_all_'
+
+
+    
 class AttendanceSerializer(serializers.ModelSerializer):
+    student = StudentSerializer()
+    total_attendance = serializers.SerializerMethodField()
     class Meta:
         model = Attendance
-        fields = ['lecture' ,'student','attend']
-        read_only_fields = ['count_attend','count_absent']
-        
+        fields = ['id','student','lecture','attend','total_attendance']
+
+    def get_total_attendance(self, obj):
+        student = obj.student
+        attend = obj.attend
+       # week=Lecture.week
+        attendance_count = Attendance.objects.filter(student=student, attend=attend).count()
+       # absent_percintage=100-(((attendance_count)/week)*100)
+        return attendance_count
