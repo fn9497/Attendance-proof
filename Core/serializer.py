4714db2f2452e883edd33228e2f815e6059dae47
fatima nasser <105967075+fn9497@users.noreply.github.com>
2023-06-07 11:41:31 +0300

    Update serializer.py

--- Core/serializer.py
+++ Core/serializer.py
@@ -1,6 +1,7 @@
 from rest_framework import serializers
 from .models import *
 from djoser.serializers import UserCreateSerializer as BaseUserCreateSerializer
+from Collage.serializers import StudentSerializer
 
 
 class AdminUserCreateSerializer(BaseUserCreateSerializer):
@@ -12,7 +13,8 @@ class UserCreateSerializer(BaseUserCreateSerializer):
         fields = ['id','username','password','email','first_name','last_name','gender','national_id','role'] 
 
 class UserSerializer(serializers.ModelSerializer):
+    student=StudentSerializer()
     class Meta:
         model = User
-        fields = ['id','username','first_name','last_name','email','gender','national_id','role']
+        fields = ['id','username','first_name','last_name','email','gender','national_id','role','student']
         read_only_fields = ['username','role']
