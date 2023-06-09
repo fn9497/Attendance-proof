a17fd43527c72e56fd5dbe3705ea5a63f4f44f47
fatima nasser <105967075+fn9497@users.noreply.github.com>
2023-06-09 20:25:02 +0300

    Update serializers.py

--- Collage/serializers.py
+++ Collage/serializers.py
@@ -1,7 +1,9 @@
 from rest_framework import serializers
-from .models import *
-from Core.serializer import UserSerializer
 
+from Tracker.models import Attendance
+from .models import * 
+from Core.serializer import UserSerializer
+from Tracker.serializers import LecturSerializer
 
 class StudentSerializer(serializers.ModelSerializer):
     class Meta:
@@ -29,10 +31,19 @@ class CourseSerializer(serializers.ModelSerializer):
 
 
 class CourseInstanceSerializer(serializers.ModelSerializer):
+    attendance_rate = serializers.SerializerMethodField()
     class Meta:
         model = CourseInstance
-        fields = ['semester','course','teacher','department','semester','year']
+        fields = ['semester', 'course', 'teacher', 'department', 'semester', 'year', 'attendance_rate']
 
+    def get_attendance_rate(self, obj):
+        total_lectures = obj.lecture_set.count()
+        total_attendance = Attendance.objects.filter(lecture__course_instance_id=obj.id, attend=1).count()
+        total_attendance = min(total_attendance, total_lectures)  # Limit attendance to total lectures count
+        if total_lectures > 0:
+            attendance_rate = (total_attendance / total_lectures) * 100
+            return attendance_rate
+        return 0
 
 class StudentCourseSerializer(serializers.ModelSerializer):
     course = CourseSerializer()
